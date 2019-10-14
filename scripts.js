2f0c7885bc5f0333d1a3921b4a0c2e243a9de58e
paulrjmsmits <paulrjmsmits@users.noreply.github.com>
2019-10-14 08:48:02 -0400

    update after initialization

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function greet()
+{
+    alert('Dear ' + document.querySelector('#name').value + ', thank you for your interest! Unfortunately our mail is not working at the moment, but you can contact me directly via paulrjmsmits@gmail.com. I hope to hear from you!');
+}
